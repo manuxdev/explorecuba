0456956470606dde18d8a53d391d5aa500ec6c92
Manuel Pantoja <manuxdev1010@gmail.com>
2023-12-27 16:01:26 -0500

    loading

--- src/app/cars/swiper.jsx
+++ src/app/cars/swiper.jsx
@@ -1,9 +1,11 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 
 import { cars } from "../data/cars";
 import Image from "next/image";
 
 const SwiperE = () => {
+  const [loading, setLoading] = useStateeState(true);
   return (
     <div className=" carousel carousel-vertical w-full sm:h-[500px] h-72">
       {cars.map((car, index) => (
@@ -11,13 +13,18 @@ const SwiperE = () => {
           key={index}
           className="carousel-item h-full flex items-center justify-center"
         >
-          <Image
-            src={car.src}
-            alt="car"
-            width={800}
-            height={200}
-            className="items-center "
-          />
+          {loading ? (
+            <span className="loading loading-dots loading-lg"></span>
+          ) : (
+            <Image
+              src={car.src}
+              alt="car"
+              width={800}
+              height={200}
+              className="items-center "
+              onLoad={() => setLoading(false)}
+            />
+          )}
         </div>
       ))}
     </div>
