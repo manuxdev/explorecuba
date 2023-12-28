222b078cf458031a879d522934c6a698e8889604
Manuel Pantoja <manuxdev1010@gmail.com>
2023-12-28 13:25:33 -0500

    delete-loader

--- src/app/cars/swiper.jsx
+++ src/app/cars/swiper.jsx
@@ -1,11 +1,7 @@
-"use client";
-import React, { useState } from "react";
-
 import { cars } from "../data/cars";
 import Image from "next/image";
 
 const SwiperE = () => {
-  const [loading, setLoading] = useState(true);
   return (
     <div className=" carousel carousel-vertical w-full sm:h-[500px] h-72">
       {cars.map((car, index) => (
@@ -13,18 +9,13 @@ const SwiperE = () => {
           key={index}
           className="carousel-item h-full flex items-center justify-center"
         >
-          {loading ? (
-            <span className="loading loading-dots loading-lg"></span>
-          ) : (
-            <Image
-              src={car.src}
-              alt="car"
-              width={800}
-              height={200}
-              className="items-center "
-              onLoad={() => setLoading(false)}
-            />
-          )}
+          <Image
+            src={car.src}
+            alt="car"
+            width={800}
+            height={200}
+            className="items-center "
+          />
         </div>
       ))}
     </div>
