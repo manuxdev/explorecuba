d1cd7c80339271e2c77d6ea36b596a6a105e44ea
Manuel Pantoja <manuxdev1010@gmail.com>
2024-01-12 19:13:52 -0500

    meta 2

--- src/app/[lang]/layout.js
+++ src/app/[lang]/layout.js
@@ -4,40 +4,41 @@ import Navbar from "./components/navbar";
 import Footer from "./components/footer";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
-import Head from "next/head";
+// import Head from "next/head";
 import { NextIntlClientProvider, useMessages } from "next-intl";
 // const inter = Inter({ subsets: ["latin"] });
 const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata = {
+  url: "https://explorecubainclassiccars.com",
+  metadataBase: new URL("https://explorecubainclassiccars.com"),
+  alternates: {
+    canonical: "/",
+  },
   title: "Explore Cuba in Classic Cars",
   description:
     "Provides information, tours, or services related to exploring classic cars in Cuba. It can be a valuable resource for enthusiasts, travellers, or individuals interested in the history and culture of Cuban classic cars.",
-  icons: {
-    icon: "/favicon.ico", // /public path
+
+  openGraph: {
+    title: "Explore Cuba in Classic Cars",
+    description:
+      "Provides information, tours, or services related to exploring classic cars in Cuba. It can be a valuable resource for enthusiasts, travellers, or individuals interested in the history and culture of Cuban classic cars.",
+    images: "/ogimage.webp",
   },
+  siteName: "Explore Cuba in Classic Cars",
 };
 
 export default function RootLayout({ children, locale }) {
   const messages = useMessages();
   return (
     <html lang={locale}>
-      <Head>
-        <meta
-          property="og:title"
-          content="Explore Cuba in Classic Cars"
-          key="title"
-        />
-        <meta
-          property="og:description"
-          content="Provides information, tours, or services related to exploring classic cars in Cuba. It can be a valuable resource for enthusiasts, travellers, or individuals interested in the history and culture of Cuban classic cars."
-          key="description"
-        />
+      <link rel="icon" href="/favicon.ico" sizes="any" />
+      {/* <Head>
         <meta property="og:image" content="/favicon.ico" key="image" />
         <meta name="twitter:card" content="summary" />
         <meta name="twitter:image" content="/favicon.ico" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-      </Head>
+      </Head> */}
       <body className={montserrat.className}>
         <NextIntlClientProvider locale={locale} messages={{ messages }}>
           <Navbar locale={locale} />
